57767fefd9b1d1271823eb85e9c688c4976f6d6a
swheatley <shaylee1@me.com>
2017-08-16 14:56:23 -0600

    Committing all fiels

--- wp-content/themes/twentyseventeen-child/single.php
+++ wp-content/themes/twentyseventeen-child/single.php
@@ -12,36 +12,32 @@
 
 get_header(); ?>
 
-<div class="wrap">
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<?php
+/* Start the Loop */
+while ( have_posts() ) : the_post();
+?>
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
-
-			$previous_post = get_previous_post();
-			echo get_permalink($previous_post -> ID);
-			die();
-
+	<div class="wrap">
+		<div id="primary" class="content-area">
+			<main id="main" class="site-main" role="main">
+				<?php
 				get_template_part( 'template-parts/post/content', get_post_format() );
+                ?>
+			</main><!-- #main -->
+		</div><!-- #primary -->
+	</div><!-- .wrap -->
+	<nav class="navigation post-navigation load-previous" role="navigation">
+		<span class="nav-subtitle">Previous post</span>
+		<div class="nav-links">
+			<div class="nav-previous">
+				<?php $previous_post = get_previous_post(); ?>
+				<a href="<?php echo get_permalink($previous_post->ID); ?>" data-id="<?php echo $previous_post->ID; ?>">
+					<?php echo $previous_post->post_title; ?>
+				</a>
+			</div>
+		</div>
+	</nav>
+<?php
+endwhile; // End of the loop.
 
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
-
-				the_post_navigation( array(
-					'prev_text' => '<span class="screen-reader-text">' . __( 'Previous Post', 'twentyseventeen' ) . '</span><span aria-hidden="true" class="nav-subtitle">' . __( 'Previous', 'twentyseventeen' ) . '</span> <span class="nav-title"><span class="nav-title-icon-wrapper">' . twentyseventeen_get_svg( array( 'icon' => 'arrow-left' ) ) . '</span>%title</span>',
-					'next_text' => '<span class="screen-reader-text">' . __( 'Next Post', 'twentyseventeen' ) . '</span><span aria-hidden="true" class="nav-subtitle">' . __( 'Next', 'twentyseventeen' ) . '</span> <span class="nav-title">%title<span class="nav-title-icon-wrapper">' . twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ) . '</span></span>',
-				) );
-
-			endwhile; // End of the loop.
-			?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-	<?php get_sidebar(); ?>
-</div><!-- .wrap -->
-
-<?php get_footer();
+get_footer();
